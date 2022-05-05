31a365c31cc0087ef5667190d119a80a5fbe2211
RoseBud13 <xiongkaijie13@gmail.com>
2022-05-06 07:28:15 +0800

    changed the router

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,10 @@ import { createWebHistory, createRouter } from "vue-router";
 const history = createWebHistory();
 
 const routes = [
-  { path: '/bubble-turntable/', meta: {title: '小王子的唱片机'}, component: () => import('../views/Home.vue') },
-  { path: '/bubble-turntable/sorrow/', meta: {title: 'void'}, component: () => import('../views/Sorrow.vue') },
-  { path: '/bubble-turntable/stars/', component: () => import('../components/Stars.vue') },
-  { path: '/bubble-turntable/rose/', component: () => import('../components/Rose.vue') }
+  { path: '/bubble-turntable', meta: {title: '小王子的唱片机'}, component: () => import('../views/Home.vue') },
+  { path: '/bubble-turntable/sorrow', meta: {title: 'void'}, component: () => import('../views/Sorrow.vue') },
+  { path: '/bubble-turntable/stars', component: () => import('../components/Stars.vue') },
+  { path: '/bubble-turntable/rose', component: () => import('../components/Rose.vue') }
 ];
 
 const router = createRouter({ history, routes });
